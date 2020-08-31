396cf02ce015fccea2aaaa94ba271448cde72504
stephensenft21 <stephensenft21@gmail.com>
2020-08-31 17:32:55 -0500

    added a new sql file with book 2 chapter 1 practice queries

---
+++ sqlPractice/practice.sql
@@ -0,0 +1,25 @@
+--  Write a query that returns the business name, city, state, and website for each dealership. 
+-- Use an alias for the Dealerships table. 
+
+-- Practice Question # 1
+-- <<
+-- SELECT 
+-- d.business_name, 
+-- d.city, 
+-- d.state, 
+-- d.website 
+-- from Dealerships d
+-- >>
+
+
+
+--  Write a query that returns the first name, last name, and email address of every customer. Use an alias for the Customers table. 
+-- Practice Question # 2
+
+-- <<
+--  SELECT 
+-- c.first_name, 
+-- c.last_name, 
+-- c.email 
+-- from Customers c    
+-- >>
