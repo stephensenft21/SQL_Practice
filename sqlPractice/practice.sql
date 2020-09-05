7578a4d7cc62b8489a75f49f3671e4f86c8e7fa4
stephensenft21 <stephensenft21@gmail.com>
2020-09-04 23:42:11 -0500

    made changes to chapter 3 practice exercises

--- sqlPractice/practice.sql
+++ sqlPractice/practice.sql
@@ -188,6 +188,9 @@ JOIN vehiclemodels mo ON vt.model_id = mo.vehicle_model_id
 
 
 
+
+-- Practice: Sales Type by Dealership
+-- 1# Produce a report that lists every dealership, the number of purchases done by each, and the number of leases done by each.
 SELECT
 d.business_name, 
 st.name, Count(s.sale_id) AS number_of_sales
@@ -198,17 +201,8 @@ JOIN salestypes st ON s.sales_type_id = st.sales_type_id
 GROUP BY d.dealership_id, st.sales_type_id
 ORDER BY d.dealership_id
 
-
-SELECT
-d.business_name, 
-st.name, Count(s.sale_id) AS number_of_sales
-FROM dealerships d 
-JOIN sales s ON s.dealership_id = d.dealership_id
-JOIN salestypes st ON s.sales_type_id = st.sales_type_id
--- WHERE st.sales_type_id = 2
-GROUP BY d.dealership_id, st.sales_type_id
-ORDER BY d.dealership_id
-
+-- Practice: Leased Types
+-- 2# Produce a report that determines the most popular vehicle model that is leased.
 
 SELECT mo.name, COUNT(s.sale_id) AS lease_count
 FROM sales s
@@ -220,7 +214,9 @@ WHERE s.sales_type_id = 2
 GROUP BY mo.vehicle_model_id
 ORDER BY COUNT(s.sale_id) DESC;
 
+--  Who Sold What
 
+-- 3# What is the most popular vehicle make in terms of number of sales?
 SELECT ma.name AS make_name, COUNT(s.sale_id) AS number_of_sales
 FROM sales s
 JOIN salestypes st ON st.sales_type_id = s.sales_type_id
@@ -229,3 +225,28 @@ JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
 JOIN vehiclemakes ma ON vt.make_id = ma.vehicle_make_id
 GROUP BY ma.vehicle_make_id
 ORDER BY COUNT(s.sale_id) DESC;
+
+
+
+
+-- 4# Which employee type sold the most of that make?
+SELECT et.name, COUNT(s.employee_id)
+FROM sales s
+JOIN vehicles v ON s.vehicle_id = v.vehicle_id
+JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
+JOIN vehiclemakes ma ON vt.make_id = ma.vehicle_make_id
+JOIN employees e ON s.employee_id = e.employee_id
+JOIN employeetypes et ON et.employee_type_id = e.employee_type_id
+WHERE ma.vehicle_make_id = 
+(
+    SELECT ma.vehicle_make_id 
+	FROM sales s
+	JOIN vehicles v ON s.vehicle_id = v.vehicle_id
+    JOIN vehicletypes vt ON v.vehicle_type_id = vt.vehicle_type_id
+    JOIN vehiclemakes ma ON vt.make_id = ma.vehicle_make_id
+    GROUP BY ma.vehicle_make_id
+	ORDER BY COUNT(s.sale_id) DESC
+	LIMIT 1
+)
+GROUP BY et.employee_type_id
+ORDER BY COUNT(s.employee_id) DESC
